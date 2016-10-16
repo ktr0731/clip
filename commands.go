73235dc2ba70f1a29025982974a1092bb311f920
Taro Aoki <aizu.s1230022@gmail.com>
2016-10-16 22:26:33 +0900

    [Add] Export methods

--- commands.go
+++ commands.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/urfave/cli"
@@ -36,8 +37,57 @@ func initClip(c *cli.Context) error {
 	return nil
 }
 
+func seekSQLiteHeader(data []byte) (int, error) {
+	header := []byte{
+		0x53, 0x51, 0x4c, 0x69, 0x74, 0x65, 0x20, 0x66, 0x6f, 0x72, 0x6d, 0x61, 0x74, 0x20, 0x33,
+	}
+
+	n, at := 0, 0
+
+	for i := range data {
+		if data[i] == header[n] {
+			if n == 0 {
+				at = i
+			}
+
+			n++
+		} else if n > 0 {
+			n = 0
+		}
+
+		if n == len(header) {
+			return at, nil
+		}
+	}
+
+	return -1, fmt.Errorf("SQLite header not found")
+}
+
+func extractSQLiteDB() error {
+	fileName, dbName := "sample.clip", "db"
+
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return fmt.Errorf("Cannot open sample.clip")
+	}
+
+	at, err := seekSQLiteHeader(data)
+
+	f, err := os.OpenFile(dbName, os.O_RDONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("Cannot open db")
+	}
+
+	f.Write(data[at:])
+
+	return nil
+}
+
 func export(c *cli.Context) error {
 	fmt.Println("Export")
+	fmt.Println("Extract db")
+
+	extractSQLiteDB()
 	return nil
 }
 
