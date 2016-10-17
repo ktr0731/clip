bd45dc9c0362edd5dd2e078ee54a271a5b9d8904
Taro Aoki <aizu.s1230022@gmail.com>
2016-10-17 16:19:12 +0900

    [Remove] Unnecessary flags

--- clip.go
+++ clip.go
@@ -10,18 +10,6 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Clip"
 	app.Usage = "The content track helper for CLIP STUDIO files"
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "lang, l",
-			Usage: "lang for ..",
-		},
-	}
-
-	app.Action = func(c *cli.Context) error {
-		return nil
-	}
-
 	app.Commands = Commands
-
 	app.Run(os.Args)
 }
